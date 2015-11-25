d6dfb0400c4e4529637364c6744fdf6ec5f7e161
Yong Christopher Tang <yong.christopher.tang@gmail.com>
2015-11-26 00:34:55 +0800

    add synchronizations to prevent contention

--- src/main/java/com/github/yongchristophertang/demo/DemoApplication.java
+++ src/main/java/com/github/yongchristophertang/demo/DemoApplication.java
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @SpringBootApplication
 public class DemoApplication {
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private static volatile boolean isServiceStarted = false;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -24,8 +26,12 @@ public class DemoApplication {
             Arrays.asList("512-780-2257", "553-780-4889")));
     }
 
-    protected final void runDemoService() throws InterruptedException, ExecutionException, TimeoutException {
+    protected synchronized final void runDemoService() throws InterruptedException, ExecutionException, TimeoutException {
+        if (isServiceStarted) {
+            return;
+        }
         executor.submit(() -> SpringApplication.run(DemoApplication.class).getStartupDate()).get(40, TimeUnit.SECONDS);
+        isServiceStarted = true;
     }
 
     protected final void stopDemoService() {
